e0f4e6835c7379c55a4f769dbc2dcfe9e45727ed
Alan Santos <Alan Ferreira dos Santos>
2024-03-05 19:50:33 -0300

    Utilizando o innerHTML

--- js/externo.js
+++ js/externo.js
@@ -1,1 +1,13 @@
-alert("Olá vindo de js externo");
+//alert("Olá vindo de js externo");
+
+// buscando um único elemento do HTML
+let container = document.querySelector("#container");
+
+// buscando vários elementos do HTML;
+let containers = document.querySelectorAll(".container");
+
+// exibindo uma mensagem no console do navegador
+console.log(container, containers);
+
+// alterado o conteúdo de um elemento
+container.innerHTML = "Elemento 1 - Texto atualizado";
