53440576e27e0210b68a8738846701e4160b0c0b
Alan Santos <Alan Ferreira dos Santos>
2024-03-05 22:50:35 -0300

    Identificação dos passos

--- js/exercicio1.js
+++ js/exercicio1.js
@@ -1,10 +1,13 @@
+// Passo 2
 let quadrado1 = document.querySelector("#resolution1 .square");
 quadrado1.style.backgroundColor = "#fdd";
 
+// Passo 3
 let quadrado2 = document.querySelector("#resolution2 .square");
 quadrado2.innerHTML = "<p>Texto adicionado</p>";
 quadrado2.style.textAlign = "center";
 
+// Passo 4
 quadrado1.onmouseover = function () {
   colocarBorda(this);
 };
@@ -17,6 +20,7 @@ function colocarBorda(elemento) {
   elemento.style.border = "2px #000 solid";
 }
 
+// Passo 5 e 6
 let botaoEsconder = document.querySelector(".btn");
 botaoEsconder.onclick = function () {
   quadrado1.classList.toggle("hide");
